b91699cda815fc608597dbc4faf4b71f738fe15b
DEADBYTE <termay10000@gmail.com>
2026-06-26 20:41:58 +0700

    tick cut

--- kernel/core.asm
+++ kernel/core.asm
@@ -86,6 +86,11 @@ run_command:
     cmp ax, 1
     je do_ver
 
+    mov di, word_tick
+    call streq
+    cmp ax, 1
+    je do_tick
+
     mov di, word_mem
     call streq
     cmp ax, 1
@@ -158,6 +163,22 @@ do_cls:
 do_ver:
     mov si, ver_msg
     call puts
+    ret
+
+do_tick:
+    mov si, tick_msg
+    call puts
+    push ax
+    push es
+    mov ax, 0x0040
+    mov es, ax
+    mov ax, [es:0x006e]
+    call put_hex16
+    mov ax, [es:0x006c]
+    call put_hex16
+    pop es
+    pop ax
+    call newline
     ret
 
 do_mem:
@@ -247,9 +268,9 @@ do_stop:
     mov si, stop_msg
     call puts
     cli
-.freeze:
+.stop_loop:
     hlt
-    jmp .freeze
+    jmp .stop_loop
 
 read_key:
 .wait:
@@ -471,14 +492,15 @@ pc_beep:
     pop ax
     ret
 
-banner db 'BONEBOX-01 v0.1.1', 10
-       db 'direct screen. direct keys. no host OS.', 10
+banner db 'BONEBOX-01 v0.1.2', 10
+       db 'direct screen. direct keys. clock sense.', 10
        db 'type help', 10, 10, 0
 prompt_msg db 'bone> ', 0
 unknown_msg db 'unknown command; type help', 10, 0
 help_msg db 'commands', 10
          db '  help   list commands', 10
          db '  ver    print version', 10
+         db '  tick   print BIOS timer counter', 10
          db '  cls    clear screen', 10
          db '  mem    show fixed memory facts', 10
          db '  ports  show touched hardware ports', 10
@@ -487,7 +509,8 @@ help_msg db 'commands', 10
          db '  demo   write pattern to B800 text memory', 10
          db '  law    print machine law', 10
          db '  stop   park CPU', 10, 0
-ver_msg db 'BONEBOX-01 v0.1.1 console cut', 10, 0
+ver_msg db 'BONEBOX-01 v0.1.2 tick cut', 10, 0
+tick_msg db 'tick 0040:006c=', 0
 mem_msg db 'boot 0000:7c00  kernel 1000:0000  vga b800:0000', 10, 0
 cs_msg db 'cs=', 0
 ports_msg db 'ports: 60/64 keyboard, 3d4/3d5 cursor, 40/42/43 PIT, 61 speaker', 10, 0
@@ -500,6 +523,7 @@ stop_msg db 'stop', 10, 0
 word_help db 'help', 0
 word_cls db 'cls', 0
 word_ver db 'ver', 0
+word_tick db 'tick', 0
 word_mem db 'mem', 0
 word_ports db 'ports', 0
 word_peek db 'peek', 0
